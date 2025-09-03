7ba0370c5ae7a4f0adf4d5e93bf45c2cf0f19b61
chang-cc27 <113362490+chang-cc27@users.noreply.github.com>
2025-09-03 23:56:43 +0800

    Create ver_1.cpp

---
+++ C++/01. Easy/104. Maximum Depth of  Binary Tree/ver_1.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if (root == 0) {
+            return 0;
+        }
+        queue<TreeNode*> q;
+        q.push(root);
+        int depth = 0;
+
+        while (!q.empty()) {
+            int size = q.size();  // 這層節點數 <==重點在這:有暨個點，for回圈跑幾次。
+            for (int i = 0; i < size; i++) {
+                TreeNode* node = q.front();
+                q.pop();
+                if (node->left) q.push(node->left);
+                if (node->right) q.push(node->right);
+            }
+            depth++;  // 每完成一層加1
+        }
+        return depth;        
+    }
+};
