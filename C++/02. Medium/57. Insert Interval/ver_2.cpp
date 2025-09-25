83d21c42331a86e53bdd428ef0ea7d4c0dcf8dba
chang-cc27 <113362490+chang-cc27@users.noreply.github.com>
2025-09-25 22:53:47 +0800

    Create ver_2.cpp

---
+++ C++/02. Medium/57. Insert Interval/ver_2.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        int n = intervals.size();
+        vector<vector<int>> ans;
+        if(n == 0) {
+            ans.push_back(newInterval);
+            return ans;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (newInterval[0] > intervals[i][1]) {
+                ans.push_back(intervals[i]);
+            } 
+            else if (newInterval[1] < intervals[i][0]) {
+                ans.push_back(newInterval);
+                ans.insert(ans.end(), intervals.begin() + i, intervals.end());
+                return ans;
+            } 
+            else {
+                newInterval[0] = min(newInterval[0], intervals[i][0]);
+                newInterval[1] = max(newInterval[1], intervals[i][1]);
+            }
+        }
+        // If last
+        ans.push_back(newInterval);
+        return ans;
+    }
+};
